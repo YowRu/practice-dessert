5aba0ed48bd615eac5989fb057d395b63498d60b
AngelaYu <love_3221213@hotmail.com>
2018-05-19 11:39:41 +0800

    copyall
    compelet

--- README.md
+++
@@ -1,2 +0,0 @@
-# practice-dessert
- Japanese dessert 
---
+++ js/loadFile.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('header').load('header.html');
+    $('aside.commodity').load('mainAside.html');
+    $('footer').load('footer.html');
+    $('#fancybox').load('fancybox.html');
+
+});   
+    
